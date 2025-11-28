f87729660ff26fe2c943d34fd2a16b555d5b7b85
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-11-28 18:52:14 +0530

    Control flow

---
+++ FRNOTEND/JAVASCRIPT/class-4.js
@@ -0,0 +1,20 @@
+//control flow
+
+// let x=11;
+// if(x%2==0){
+//   console.log("True this is even");
+// }else{
+//   console.log("False this is Odd");
+// }
+
+// let value = 8;
+// switch(value)
+// {
+//   case 1: console.log("Hii-1"); break;
+//    case 2: console.log("Hii-2"); break;
+//     case 3: console.log("Hii-3"); break;
+//      case 4: console.log("Hii-4"); break;
+//       case 5: console.log("Hii-5"); break;
+//        case 6: console.log("Hii-6"); break;
+//       default: console.log("put value right")
+// }
