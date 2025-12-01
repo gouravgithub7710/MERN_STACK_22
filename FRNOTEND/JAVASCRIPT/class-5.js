9e53f4a39bd006dd9bf85e40724d5615944b37b2
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-01 15:36:56 +0530

    class-5-Loops

---
+++ FRNOTEND/JAVASCRIPT/class-5.js
@@ -0,0 +1,37 @@
+// Loops
+
+//while
+//Do while
+//for loop
+//for in
+//for of
+
+// their are mainly three types of loops - while and do while and for
+
+// 1-for
+
+// for(let i = 0;i<5;i++)
+// {
+//   console.log("Num is :"+i);  
+// }
+
+// 2-while
+
+// let x=0;
+// let y=4;
+// while(x<=y){
+// console.log("num is :"+x);
+// x++;
+// }
+
+// 3-Do while
+
+// let x=0;
+// let y=4;
+// do{
+//   console.log("Hello, I'm do wala section");
+//   x++;
+// }while(x<=y){
+//  console.log("num is :"+x);
+ 
+// }
