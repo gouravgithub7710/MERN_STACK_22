22a2e2a4f2f726f5824c116ab431ede3c864690b
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-01 16:00:32 +0530

    Reverse

--- FRNOTEND/JAVASCRIPT/class-6.js
+++ FRNOTEND/JAVASCRIPT/class-6.js
@@ -1,22 +1,42 @@
 // JS-Loops Q
 
-// Palindrome
+// � EVEN / ODD
 
-let num = 1221;
-let result = 0;
-let cpyNum = num;
-let reminder=0;
+//1. Write a program to check whether a number is Even or Odd.
 
-while(num>0){
-  reminder = num%10;
-result = result*10+reminder;
-num = Math.floor(num/10);
-}
 
-if(cpyNum == result)
-{
-  console.log(`Num is ${cpyNum} is Palindrome`); 
-}else{
-  console.log(`Num is ${cpyNum} is not Palindrome`); 
-}
+
+// Palindrome----------------------------------------------------------------
+
+// let num = 1221;
+// let result = 0;
+// let cpyNum = num;
+// let reminder=0;
+
+// while(num>0){
+//   reminder = num%10;
+// result = result*10+reminder;
+// num = Math.floor(num/10);
+// }
+
+// if(cpyNum == result)
+// {
+//   console.log(`Num is ${cpyNum} is Palindrome`); 
+// }else{
+//   console.log(`Num is ${cpyNum} is not Palindrome`); 
+// }
+
+
+//Reverse Number------------------------------------------------------------------
+
+// let num = 12345;
+// let reverse = 0;
+// let reminder=0;
+
+// while(num>0){
+//   reminder = num%10;
+// reverse = reverse*10+reminder;
+// num = Math.floor(num/10);
+// }
+// console.log(`Reverse num is ${reverse}`); 
 
