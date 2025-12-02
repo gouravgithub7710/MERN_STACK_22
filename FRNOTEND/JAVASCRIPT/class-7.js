90d41e026a3595a78222221ab0339fed37c82226
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-02 18:54:20 +0530

    class-7-logical-Q

---
+++ FRNOTEND/JAVASCRIPT/class-7.js
@@ -0,0 +1,65 @@
+
+// Patterns
+
+// let r=4;
+// let c=4;
+
+// let ans="";
+
+// for(let i=0;i<=r;i++)
+// {
+//   for(let j=0;j<=c;j++)
+//   {
+//     ans=ans+"x"; 
+//   }
+//  console.log(ans);
+//  ans=" ";
+// }
+
+
+// let r=4;
+// let c=4;
+
+// let ans=" ";
+
+// for(let i=4;i>=0;i--)
+// {
+//   for(let j=0;j<=i;j++)
+//   {
+//     ans=ans+"x"; 
+//   }
+//  console.log(ans);
+//  ans=" ";
+// }
+
+
+// let r=4;
+// let c=4;
+
+// let ans="";
+
+// for(let i=0;i<=r;i++)
+// {
+//   for(let j=0;j<=c;j++)
+//   {
+//     ans=ans+"x"; 
+//   }
+//  console.log(ans);
+//  ans=" ";
+// }
+
+
+let r=4;
+let c=4;
+
+let ans="";
+
+for(let i=0;i<=r;i++)
+{
+  for(let j=0;j<=c;j++)
+  {
+    ans=ans+"x"; 
+  }
+ console.log(ans);
+ ans=" ";
+}
