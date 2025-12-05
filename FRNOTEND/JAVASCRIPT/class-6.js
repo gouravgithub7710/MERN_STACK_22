900b6bee086e4ac0fd547584269696521c0d6a17
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-05 17:48:18 +0530

    hof , callback and hosting

--- FRNOTEND/JAVASCRIPT/class-6.js
+++ FRNOTEND/JAVASCRIPT/class-6.js
@@ -532,3 +532,115 @@
 // console.log("difference between sum of even digits and sum of odd digits is:"+difference);
 
 
+
+
+
+
+// function callback(x,y){
+//     return x+y;
+// }
+
+// function higheror(sum){
+//   let x =5;
+//   let y=10;
+
+//   let add = sum(x,y);
+//   console.log(add);
+// }
+
+
+// higheror(callback);
+
+
+
+//funciton hosting ---------------
+// greet();  // This works
+
+// function greet() {
+//   console.log("Hello Hoisting!");
+// }
+
+
+// higherorder and cllback function--------------
+
+// function higherOrder(fn) {
+ 
+//   let x = 5;
+//   let y = 10;
+//   let ans = fn(x,y);
+//   console.log(ans);
+  
+// }
+
+// function sayHello(x,y) {
+    
+//   console.log("Hello from Callback");
+//   return x+y;
+// }
+
+// higherOrder(sayHello);
+
+
+// console.log(x)
+// function x(){
+//     console.log("hii");
+// }
+
+// let x =(a,b)=>  {
+//     console.log(a)
+//     console.log(b)
+// };
+// let ans =x(2,4);
+// console.log(ans)
+
+// console.log(x);
+// var x=5;
+
+
+// console.log(x)
+// function x(){
+//     console.log("fn")
+// }
+
+// console.log(x())
+// let x = function f(){
+//     console.log("hii")
+// }
+
+// greet();   // Works
+
+// function greet() {
+//   console.log("Hello Function Hoisting");
+// }
+
+
+// hello();   // ❌ TypeError
+// var hello = function() {
+//   console.log("Hi");
+// };
+
+// hello();   // ❌ refrece error 
+// let hello = function() {
+//   console.log("Hi");
+// };
+
+// console.log(hello());   // ❌ refrence error
+// const hello = function() {
+//   console.log("Hi");
+// };
+
+
+
+// test();   // ❌ ReferenceError
+// let test = () => console.log("test");
+
+
+  var num = 10;
+
+// function display() {
+//   console.log(num);
+// //   var num = 20;
+// }
+
+// display();
+
