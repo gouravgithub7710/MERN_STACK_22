849d7ca52e6b55c35a598ea89974b6187b431699
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-06 17:48:12 +0530

    Hosting and higherorder

---
+++ FRNOTEND/JAVASCRIPT/JS-Interview-code/Hosting.js
@@ -0,0 +1,82 @@
+
+//funciton hosting ---------------
+
+
+// greet();  // This works
+
+// function greet() {
+//   console.log("Hello Hoisting!");
+// }
+
+
+
+
+// console.log(x)
+// function x(){
+//     console.log("hii");
+// }
+
+
+
+// let x =(a,b)=>  {
+//     console.log(a)
+//     console.log(b)
+// };
+// let ans =x(2,4);
+// console.log(ans)
+
+// console.log(x);
+// var x=5;
+
+
+// console.log(x)
+// function x(){
+//     console.log("fn")
+// }
+
+
+
+// console.log(x())
+// let x = function f(){
+//     console.log("hii")
+// }
+
+
+
+// greet();   // Works
+
+// function greet() {
+//   console.log("Hello Function Hoisting");
+// }
+
+
+// hello();   // ❌ TypeError
+// var hello = function() {
+//   console.log("Hi");
+// };
+
+// hello();   // ❌ refrece error 
+// let hello = function() {
+//   console.log("Hi");
+// };
+
+// console.log(hello());   // ❌ refrence error
+// const hello = function() {
+//   console.log("Hi");
+// };
+
+
+
+// test();   // ❌ ReferenceError
+// let test = () => console.log("test");
+
+
+//   var num = 10;
+
+// function display() {
+//   console.log(num);
+// //   var num = 20;
+// }
+
+// display();
+
