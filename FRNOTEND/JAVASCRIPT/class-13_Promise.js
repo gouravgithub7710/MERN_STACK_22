0d0e46354559ca01dc7fe4f3065215d0afb85da0
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-11 17:07:15 +0530

    Class-13-Promise

---
+++ FRNOTEND/JAVASCRIPT/class-13_Promise.js
@@ -0,0 +1,22 @@
+//Promise:
+          // is solution for the Callback hell
+
+
+       //defination 
+       //promise:- is a object in js 
+                  // which is give assurance/grauntee
+                  //i will give you somtheing 
+
+                  //isme 3 stage hote hote 
+                      //1:pending (defalutt)
+                      //2: resolve (fullfiled)
+                      //3: reject  (error)
+
+          //const p = new promise((resolve,reject)=>{LOGIC});
+
+                  //first parameter is always  RESOLVE then REJECT
+
+                  //where RESOLVE for then()
+                  //and
+                  //REJECT for Cath()
+
