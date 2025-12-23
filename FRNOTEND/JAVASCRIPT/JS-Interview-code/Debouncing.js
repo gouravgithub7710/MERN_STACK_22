d5a117c2c4fc812257a7b566ba0426969f95160e
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-23 22:46:42 +0530

    Deboucing

---
+++ FRNOTEND/JAVASCRIPT/JS-Interview-code/Debouncing.js
@@ -0,0 +1,14 @@
+//Debouncing-----------------------------
+
+// Debouncing means calling a function only after the user stops doing an action for some time.
+
+        // JavaScript me debouncing ka matlab hai function ko tab run karna jab kuch time tak event dobara trigger na ho.
+
+        // Debouncing ek technique hai jisme kisi function ko tabhi run karte hain jab user kuch time tak repeat action (click / type) band kar de.
+
+// Simple example (real life)
+
+        // Search box me user fast-fast type karta hai → debouncing wait karta hai → jab typing rukti hai tab API call hoti hai.
+
+
+
