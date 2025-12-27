140e602c9e38971291ff8cb336d76d176b6f28cc
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-27 14:49:38 +0530

    function

---
+++ FRNOTEND/JAVASCRIPT/JS-Interview-code/Function.js
@@ -0,0 +1,38 @@
+// function statment or function decleration  ----------------
+function a(){
+  console.log("a called");
+}
+
+// function expression ----------------
+ 
+var b = function () {
+console.log("b called")
+}
+
+// Anonymous funciton --------------
+    //function without name 
+// function () {
+// }
+
+
+// Named Function ---------------
+// var b = function xyz () {
+// console.log("b called")
+// }
+
+// parameter
+function sum(a,b){
+  console.log("sum",a+b);
+}
+ 
+sum(4,4)//argument 
+
+
+//first class function 
+                        // : the abality of function to used as a value and it can be passed as argument to another function and can be returned from the funciton  this abality know as first class fn .
+
+
+// arrow fn
+
+
+
