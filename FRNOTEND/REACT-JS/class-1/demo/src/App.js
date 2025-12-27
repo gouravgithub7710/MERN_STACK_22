6a27d92a5638ff22cd22bd930494cf39efb7a6c3
gouravgithub7710 <gouravgiri2004@gmail.com>
2025-12-27 14:52:14 +0530

    React Start

---
+++ FRNOTEND/REACT-JS/class-1/demo/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { useEffect } from 'react';
+
+function App() {
+ 
+  // const [userName, setUserName] = useState("Speed UP")
+  const [Info, setInfo] = useState([])
+  
+  useEffect( async () => {
+    
+    const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+     const ans =   await res.json();
+     setInfo(ans);
+   
+  }, [])
+  
+
+  return (
+    <div>
+
+   <p>   {Info[0]?.title}</p>
+
+   {Info.map((data)=>{ return <p>{data.title}</p>})}
+      
+
+
+    {/* <h1>hello</h1>
+    {userName}
+        <button onClick={()=>{
+          setUserName("Career dubo diya")
+        }}>Change Speed</button> */}
+
+    </div>
+
+  );
+}
+
+export default App;
