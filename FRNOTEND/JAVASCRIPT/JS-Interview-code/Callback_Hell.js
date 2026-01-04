9fcfe82d7bae93769c4aff06f76d488a1f992abf
gouravgithub7710 <gouravgiri2004@gmail.com>
2026-01-04 21:41:12 +0530

    callback hell

--- FRNOTEND/JAVASCRIPT/JS-Interview-code/Callback_Hell.js
+++ FRNOTEND/JAVASCRIPT/JS-Interview-code/Callback_Hell.js
@@ -1,5 +1,30 @@
 //callback_hell
 
+// having problem in callback - 
+// 1: callback-hell, 2: inversion of control
+
+
+const cart = ["shops","pants","kurta"];
+
+api.createOrder(cart, function(){
+
+    api.proceedToPayment(function(){
+
+        api.showOrderSummery(function(){
+
+            api.updateWallet()
+        })
+    })
+})
+
+// this above is callback hell and in this we are invesrion of control our proceedToPayment to createOrder means we give control to createOrder.
+
+// we can solve this problem using promise
+
+
+
+g
+
 function callback_hell(method, url, next) {
   const ob1 = new XMLHttpRequest();
   ob1.responseType = "json";
